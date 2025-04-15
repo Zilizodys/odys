3428b395e41b1892c978b4b8db7598a4ead2fb15
Odys <contact@odys.ai>
2025-04-15 12:54:28 +0200

    perf: Optimisation de la configuration webpack et résolution des problèmes de cache

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   poweredByHeader: false,
   images: {
     domains: ['images.unsplash.com'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Optimisations webpack personnalisées ici
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
   }
 }
 
