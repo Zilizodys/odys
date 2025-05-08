bb65ce088e4163a6cfb73635463da3617083e456
Odys <contact@odys.ai>
2025-05-09 00:49:45 +0200

    fix: correction du typage des props du layout

--- src/app/program/[id]/layout.tsx
+++ src/app/program/[id]/layout.tsx
@@ -1,16 +1,18 @@
 import { Metadata } from 'next'
 
-type Props = {
-  params: { id: string }
+type LayoutProps = {
   children: React.ReactNode
+  params: {
+    id: string
+  }
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: LayoutProps): Promise<Metadata> {
   return {
     title: `Programme ${params.id}`,
   }
 }
 
-export default function ProgramLayout({ children }: Props) {
+export default function ProgramLayout({ children }: LayoutProps) {
   return children
 } 
